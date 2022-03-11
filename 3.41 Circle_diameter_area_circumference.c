c4115737bc7f4f04da3af48a30f45960b7682885
Semicide <190101033@ogr.atu.edu.tr>
2022-03-12 01:47:03 +0300

    Create 3.41 Circle_diameter_area_circumference.c

---
+++ 3.41 Circle_diameter_area_circumference.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+Program:circle-3.41
+Purpose:Finding diameter,circumference and area of a circle with its radius
+Author:Semicide
+11/03/2022
+
+*******************************************************************************/
+#include <stdio.h>
+
+float main(){
+    float r;
+    float pi=3.14159;
+    printf("please enter the radius ");
+    scanf("%f",&r);
+    printf("diameter=%f",2*r);
+    printf("\ncircumference=%f",2*pi*r);
+    printf("\narea=%f",pi*r);
+    
+        
+    }
+    
+ 
